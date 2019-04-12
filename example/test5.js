ff6d2f770f9895be2ac2e947697b8f76e488cb37
Levente Morva <levente95@gmail.com>
2019-04-12 21:02:54 +0200

    fix: move current position when a path is closed (#9)

---
+++ example/test5.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+var canvas = document.getElementById('can5');
+var ctx = canvas.getContext('2d')
+
+var path = new Path2D('M20,20h100v100Zm120,0h100v100Z');
+
+ctx.strokeStyle = '#666';
+ctx.fillStyle = '#DDD';
+
+ctx.fill(path);
+ctx.stroke(path);
--- src/path2d-polyfill.js
+++ src/path2d-polyfill.js
@@ -115,6 +115,7 @@ function polyFillPath2D(window) {
     let qcpx;
     let qcpy;
     let ccw;
+    let startPoint = { x: 0, y: 0 };
     const currentPoint = { x: 0, y: 0 };
 
     canvas.beginPath();
@@ -135,13 +136,19 @@ function polyFillPath2D(window) {
 
       switch (pathType) {
         case 'm':
-          x += s[1];
-          y += s[2];
-          canvas.moveTo(x, y);
-          break;
         case 'M':
-          x = s[1];
-          y = s[2];
+          if (pathType === 'm') {
+            x += s[1];
+            y += s[2];
+          } else {
+            x = s[1];
+            y = s[2];
+          }
+
+          if (pathType === 'M' || !startPoint) {
+            startPoint = { x, y };
+          }
+
           canvas.moveTo(x, y);
           break;
         case 'l':
@@ -317,6 +324,9 @@ function polyFillPath2D(window) {
           break;
         case 'z':
         case 'Z':
+          x = startPoint.x;
+          y = startPoint.y;
+          startPoint = undefined;
           canvas.closePath();
           break;
         case 'AC': // arc
@@ -341,6 +351,7 @@ function polyFillPath2D(window) {
           y = s[2];
           w = s[3];
           h = s[4];
+          startPoint = { x, y };
           canvas.rect(x, y, w, h);
           break;
         default:
